2bde67cf7ef128fae6b137d4ea24fcd04f4503e1
Emre Tezisci <emretezisci@gmail.com>
2020-04-25 18:26:25 +0100

    Added seat selection

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,79 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          <img
+            src="./assets/img/bus.png"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt
+          />
+          Ticket Reservation System
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link to="/home" class="nav-link" active-class="active">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="search-trip" class="nav-link" active-class="active">Search Trip</router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Select Seat</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Passenger Info</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Payment Info</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700,700i&subset=latin-ext");
+@import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+@import "~bootstrap/scss/bootstrap";
+
+body {
+  font-family: "Montserrat";
+  font-size: 18px;
 }
 
-#nav {
-  padding: 30px;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: 600;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.form-control:focus {
+  outline: none;
+  box-shadow: none;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.navbar {
+  margin-bottom: 20px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,43 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import SearchTrip from "../views/SearchTrip.vue";
+import SeatSelection from "../views/SeatSelection.vue"
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    alias: "/home",
+    name: "Home",
+    redirect: "search-trip",
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/search-trip",
+    name: "search-trip",
+    component: SearchTrip,
+  },
+  {
+    path: "/seat-selection/:trip_id?",
+    name: "seat-selection",
+    component: SeatSelection,
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
